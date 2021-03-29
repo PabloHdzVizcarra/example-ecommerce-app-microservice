f8c1ea30bd7f8a06dba374b9e4bc680d4a13b443
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-03-29 11:12:15 -0600

    feat create controller route api/create-order

--- Order/src/index.ts
+++ Order/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import { orderController } from './infrastucture/infrastructure'
 
 const app = express()
 app.use(express.json())
 
+app.post('/api/create-order', orderController)
 
 app.listen(4200, () => {
   console.log('Order Service listening in port 4200')
